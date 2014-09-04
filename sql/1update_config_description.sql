0c5e36c5020c4ea1cedeb77b1b4b9c2a156a274d
abumuhammadx <abumuhammadx@xubuntu1404>
2014-09-04 15:57:33 +0700

    First Commit: Hapus folder /sql, tidak diperlukan

--- sql/1update_config_description.sql
+++
@@ -1,7 +0,0 @@
-#update config
-update config set description = 'Nama Toko' where `option`='store_name';
-update config set description = 'Struk Footer 1' where `option`='receipt_footer1';
-update config set description = 'Struk Footer 2' where `option`='receipt_footer2';
-update config set description = 'Struk Header 1' where `option`='receipt_header1';
-update config set description = 'Temporary Space' where `option`='temporary_space';
-update config set description = 'Versi' where `option`='version';
