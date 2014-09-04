afb2278d8d80f17799ec74b44418905836c69013
Abu Muhammad X <b.hermianto@gmail.com>
2014-09-04 16:52:13 +0700

    Upgrade file-file ahadpos2

---
+++ sistem/modul/mod_diskon.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Diskon digabung di modul barang
+ */
+
+/*
+check_user_access(basename($_SERVER['SCRIPT_NAME']));
+session_start();
+
+$act = $_GET['act'];
+
+switch ($act) :
+	case 'input':
+		break;
+endswitch;
+ * 
+ */
