f9a0bdbf6d383addb17b982ba2fe6b9dada161f4
Abu Muhammad <b.hermianto@gmail.com>
2014-12-03 12:19:15 +0700

    bersih-bersih

--- sistem/modul/mod_diskon.php
+++ sistem/modul/mod_diskon.php
@@ -2,16 +2,3 @@
 /*
  * Diskon digabung di modul barang
  */
-
-/*
-check_user_access(basename($_SERVER['SCRIPT_NAME']));
-session_start();
-
-$act = $_GET['act'];
-
-switch ($act) :
-	case 'input':
-		break;
-endswitch;
- * 
- */
