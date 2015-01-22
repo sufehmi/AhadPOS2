ea1b0721fa95684d1215e48c1d4e438420cd4125
Abu Muhammad <b.hermianto@gmail.com>
2015-01-22 15:22:24 +0700

    Menambahkan modul membership

---
+++ sistem/modul/mod_membership.php
@@ -0,0 +1,19 @@
+<?php
+/* mod_customer.php ------------------------------------------------------
+  version: 1.01
+
+  Part of AhadPOS : http://ahadpos.com
+  License: GPL v2
+  http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+  http://vlsm.org/etc/gpl-unofficial.id.html
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License v2 (links provided above) for more details.
+  ---------------------------------------------------------------- */
+
+include "../config/config.php";
+check_user_access(basename($_SERVER['SCRIPT_NAME']));
+
+
