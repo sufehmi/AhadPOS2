cf371e44f784a9cd7c3b59f0e2294b382eed6e45
Abu Muhammad <b.hermianto@gmail.com>
2015-02-03 10:11:08 +0700

    Diskon: Menambah comment untuk diskon 1002

--- sistem/modul/mod_barang.php
+++ sistem/modul/mod_barang.php
@@ -2165,7 +2165,7 @@ switch ($_GET['act']) {
                         <script type="text/javascript">
                             $("#diskonTipeId").change(function () {
                                 var diskonId = $(this).val();
-                                // 1000:gudang; 10001:waktu
+                                // 1000:gudang; 1001:waktu; 1002: waktu khusus member
                                 if (diskonId == 1000) {
                                     $(".show-on-grosir-only").show();
                                     $(".show-on-waktu-only").hide();
