7268ae8f669ea462da270e29892102559595d204
tacoen <tacoen@gmail.com>
2016-03-03 09:30:23 +0900

    Update define.php
    delete beta/test parameter

--- define.php
+++ define.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
 
-$beta = '/2c';
+$beta = '';
 if (!defined('SITE_URL')) { DEFINE ('SITE_URL',"http://".$_SERVER['SERVER_NAME'].$beta."/"); }
 if (!defined('SITE_ROOT')) { DEFINE ('SITE_ROOT',$_SERVER["DOCUMENT_ROOT"].$beta."/"); }
 if (!defined('BRAND_NAME')) { DEFINE ('BRAND_NAME','Ahad POS'); }
